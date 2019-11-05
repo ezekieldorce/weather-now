670c565f58e95e1079ef4acde219ad764df83e76
Ezekiel <ezekieldorce@gmail.com>
2019-11-05 11:04:08 -0500

    fixed app level middleware

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const app = express()
- 
+const bodyParser = require('body-parser');
+
 app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 //GET
 app.get('/', function (req, res) {
   res.render('home.ejs');
 })
+
+app.post('/', function (req, res){
+  console.log('Hitting Post Route')
+  console.log(req.body.city);
+  res.end();
+});
  
 app.listen(3000, function(){
     console.log('Server is live on port: 3000');
