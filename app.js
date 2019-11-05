71806d23621ec81a318d91fe1f5f293bad003eda
Ezekiel <ezekieldorce@gmail.com>
2019-11-05 11:50:37 -0500

    Added Data Binding (still needs work)

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //GET
 app.get('/', function (req, res) {
-  res.render('home.ejs');
-})
+    res.render('home.ejs', {weather: 'coming soon'});
+});
 
 app.post('/', function (req, res){
   console.log('Hitting Post Route')
   console.log(req.body.city);
-  res.end();
-});
+  let weatherNow= "The weather in " + req.body.city + "is 70 C"
+  let err= "There was an error, please try again."
+  res.render('home.ejs', {weather: weatherNow, error: err});
+  });
  
 app.listen(3000, function(){
     console.log('Server is live on port: 3000');
