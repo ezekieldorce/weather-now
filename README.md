7f2689b62a20045a35e42012669eebb6ed74f3c1
Ezekiel <ezekieldorce@gmail.com>
2019-11-05 15:56:55 -0500

    Kinda done but this thing makes me wanna ri-

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # weather-now
+
+<!-- get user input of city they want weather results from -->
+
+<!-- Get weather for the city the user inputs -->]
+    if error, then let the user know that there was a probleml;
+If there's no weather available, show error.
+    else store the weather of city
+    Show weather of storeddc
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const request= require('request')
+const apiKey= process.env.APIKEY;
+console.log(process.env.APIKEY);
+
 
 app.set('view engine', 'ejs');
 
@@ -10,17 +15,28 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //GET
 app.get('/', function (req, res) {
-    res.render('home.ejs', {weather: 'coming soon'});
+    res.render('home.ejs', {weather: null, error: null});
 });
 
 app.post('/', function (req, res){
-  console.log('Hitting Post Route')
-  console.log(req.body.city);
-  let weatherNow= "The weather in " + req.body.city + "is 70 C"
-  let err= "There was an error, please try again."
-  res.render('home.ejs', {weather: weatherNow, error: err});
+
+
+  let url= 'http://api.openweathermap.org/data/2.5/weather?q='+ req.body.city + '&units=imperial&APPID=weather?q' + apiKey;
+
+request(url, function (error, response, body) {
+  if(error){
+    res.render('home.ejs', {weather: null, error: "Error, please try again"})
+  } else {
+    let weather=(JSON.parse(body));
+    if(weather.main == undefined){
+      res.render('home.ejs', {weather: null, error: "Error, please try again"})
+     } else {
+        let weatherNow= "The weather in " + req.body.city + " is" + weather.main.temp + " degrees";
+        res.render('home.ejs', {weather: weatherNow, error: null } );
+      }
+    }
   });
- 
+});
 app.listen(3000, function(){
     console.log('Server is live on port: 3000');
 });
